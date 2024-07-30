41ce81e63c18397d934c4146d89bc8363297b867
Joshua Carroll <carrolljosh1976@gmail.com>
2024-07-29 21:57:36 -0400

    Added loading page for NewsDetailPage

---
+++ app/(content)/news/[slug]/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NewsNotFoundPage() {
+    return (
+        <div id="error">
+            <h1>Article Not Found!</h1>
+            <p>The requested news resource could not be found!</p>
+        </div>
+    )
+}
